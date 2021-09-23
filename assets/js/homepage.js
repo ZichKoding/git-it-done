1d337478767beb82eb0f1966913d9fe5ac60ec88
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-23 13:00:18 -0400

    6.2.5

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,7 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +9,27 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
 
-getUserRepos("octocat");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input elemnet 
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
